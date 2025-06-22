1148325ba22eaf0e72e137bea6e4a2f3e05ba2df
LM10essi <741260264@qq.com>
2025-06-22 22:27:06 +0800

    2

--- src/main/java/szuc/kqn/GlobalMemory.java
+++ src/main/java/szuc/kqn/GlobalMemory.java
@@ -8,4 +8,5 @@ import java.util.concurrent.CopyOnWriteArrayList;
 public class GlobalMemory {
     public static ConcurrentHashMap<Objects,Objects> map=new ConcurrentHashMap<>();
     public static CopyOnWriteArrayList<Objects> list=new CopyOnWriteArrayList<>();
+    public static Object variable=new Object();
 }
